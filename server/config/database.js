0244327bb3bad4ebd1c334f5cfc9d091cb0a877d
harshvaghasiya_webelight <harshvaghasiya@webelight.co.in>
2024-05-11 16:53:17 +0530

    old code removed

--- server/config/database.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDb = async () => {
-  const { connection } = await mongoose.connect(process.env.DB_URL);
-  console.log("Mongodb Connected with", connection.host);
-};
